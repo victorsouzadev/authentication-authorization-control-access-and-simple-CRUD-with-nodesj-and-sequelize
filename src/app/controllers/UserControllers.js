2009e2a6ece7dbb477bc77101b68cfe25c035cb2
Victor Souza <victoor.soouza@gmail.com>
2020-09-30 16:09:42 -0300

    refactor: Atualização das mensagens de sucesso

--- src/app/controllers/UserControllers.js
+++ src/app/controllers/UserControllers.js
@@ -16,18 +16,18 @@ class UserController {
     }
     async delete(req,res){
        return await User.destroy({where:{id:req.params.id}})
-           .then(user=> res.status(200).json(user))
+           .then(user=> res.status(200).json({'user':user,"message":"success delete"}))
            .catch(erro=> res.status(500).json(erro))
     }
     async update(req,res){
         if(req.body.password){
             const password_hash = await bcrypt.hash(req.body.password,8)
             return await User.update({...req.body,password_hash:password_hash},{where:{id:req.params.id}})
-            .then(user=> res.status(200).json(user))
+            .then(user=> res.status(200).json({'user':user,"message":"success update"}))
             .catch(erro=> res.status(500).json(erro))
         }
         return await User.update(req.body,{where:{id:req.params.id}})
-            .then(user=> res.status(200).json(user))
+            .then(user=> res.status(200).json({'user':user,"message":"success update"}))
             .catch(erro=> res.status(500).json(erro))
         
     }
